9b467f65a9fcde067f70c4d0359afcf8f2089f25
rahul sawhney <you@example.com>
2023-05-04 11:13:03 +0530

    upated changes

--- abha/src/main/java/com/abym/abha/Constants/AppConstants.java
+++ abha/src/main/java/com/abym/abha/Constants/AppConstants.java
@@ -2,10 +2,6 @@ package com.abym.abha.Constants;
 
 public class AppConstants {
 
-    public static String CLIENT_ID = "";
-    public static String CLIENT_SECRET = "";
-    public static boolean IS_VERIFIED = false;
-    public static String APPMODE = "mode";
     public static String PROD = "production";
     public static String UAT = "development";
     public static String MOBILENO = "mobileNo";
